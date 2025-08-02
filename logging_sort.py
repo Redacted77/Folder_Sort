4c7620473535df05636e00a6069bb1a7f381d295
Hazem Abdelrahm Rizk <166548681+Redacted77@users.noreply.github.com>
2025-08-02 06:42:10 +0300

    Add files via upload
    Logs now handle non-English file names correctly.

--- logging_sort.py
+++ logging_sort.py
@@ -11,8 +11,8 @@ class logs:
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
         with open(self.log_text_file, 'w', encoding="utf-8") as L:
-            L.write(f"Logging start --- [{t}]\n")
-            L.write(f"monitoring: {folder_path} --- {t}\n")
+            L.write(f"Logging start  ---  [{t}]\n")
+            L.write(f"monitoring: {folder_path}  ---  {t}\n")
             L.write("----------------\n")
             L.write("\n")
             
@@ -20,40 +20,42 @@ class logs:
     def call_to_check_all(self, path):
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.log_text_file, 'a', encoding="utf-8") as L:
-            L.write(f"Checked: {path} --- [{t}]\n")
+            L.write(f"Checked: {path}  ---  [{t}]\n")
             L.write("\n")
 
     # logs the stray files found in the folder
     def stary_files_logs(self, stray_file_list: list):
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.log_text_file, 'a', encoding="utf-8") as L:
-            L.write(f"Stray files: {stray_file_list} --- [{t}]\n")
+            L.write(f"Stray files:  \n")
+            for index, item in enumerate(stray_file_list):
+                L.write(f"{index+1}. {item}  ---  [{t}]\n")
             L.write("\n")
 
     # logs the moved files, berfore and after location
     def on_move(self, id, old_location, new_location):
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.log_text_file, 'a', encoding="utf-8") as L:
-            L.write(f"Moved: {old_location} --> {new_location} ---  call id: {id} --- [{t}]\n")
+            L.write(f"Moved: {old_location} --> {new_location} ---  call id: {id}  ---  [{t}]\n")
             L.write("\n")
 
     # logs the created folders
     def on_folder_create(self, path):
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.log_text_file, 'a', encoding="utf-8") as L:
-            L.write(f"Created: {path} --- [{t}]\n")
+            L.write(f"Created: {path}  ---  [{t}]\n")
             L.write("\n")
 
     # logs the renaming of files, old name and new name
     def on_rename(self, id, old_name, new_name):
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.log_text_file, 'a', encoding="utf-8") as L:
-            L.write(f"Renamed: {old_name} to {new_name} --- call id: {id} --- [{t}]\n")
+            L.write(f"Renamed: {old_name} to {new_name} --- call id: {id}  ---  [{t}]\n")
             L.write("\n")
 
     # incase of a specific loggin event (errors and the like)
     def other(self, text):
         t = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.log_text_file, 'a', encoding="utf-8") as L:
-            L.write(f"{text} --- [{t}]\n")
+            L.write(f"{text}  ---  [{t}]\n")
             L.write("\n")
